8e19d9cbaed3c9e90b447e0d9ee650b384671784
SkyfallWasTaken <mahadkalam1@proton.me>
2024-03-30 12:49:28 +0000

    Add color_eyre, tracing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
+use color_eyre::{eyre::Context, Result};
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
 use std::net::TcpListener;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+fn main() -> Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)
+        .wrap_err("setting default subscriber failed")?;
+
+    let listener = TcpListener::bind("127.0.0.1:6379")?;
+
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
-                println!("accepted new connection");
+                debug!("accepted new connection");
             }
             Err(e) => {
-                println!("error: {}", e);
+                error!("{e}");
             }
         }
     }
+
+    Ok(())
 }
